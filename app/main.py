598cddf468bca95f22a3f81ee3be91696419d40a
NEP-ENG\ehsan.ghorbani <ehsan.ghorbani@neptronic.com>
2025-12-04 09:05:18 -0500

    [implement exit]

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def main():
         command = input()
         print(f"{command}: command not found")
         # pass
-        exit()
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
