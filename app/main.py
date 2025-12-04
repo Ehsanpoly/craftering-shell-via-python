26c0d10775393b3ab4c6bf19c9865e823d30491c
NEP-ENG\ehsan.ghorbani <ehsan.ghorbani@neptronic.com>
2025-12-04 07:38:36 -0500

    [fix test failure]

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 import sys
 
 
+
+
 def main():
-    # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
-    pass
-
-command = input()
-
-print(f"{command}: command not found")
-
+    while True:
+         # TODO: Uncomment the code below to pass the first stage
+        sys.stdout.write("$ ")
+        command = input()
+        print(f"{command}: command not found")
+        pass
 
 if __name__ == "__main__":
     main()
