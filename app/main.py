575169179bbc7e294dd0f68f0b793ea52333f46e
NEP-ENG\ehsan.ghorbani <ehsan.ghorbani@neptronic.com>
2025-12-04 08:02:35 -0500

    [Implement REPL]

--- app/main.py
+++ app/main.py
@@ -6,10 +6,11 @@ import sys
 def main():
     while True:
          # TODO: Uncomment the code below to pass the first stage
+        
         sys.stdout.write("$ ")
         command = input()
         print(f"{command}: command not found")
-        pass
+        # pass
 
 if __name__ == "__main__":
     main()
