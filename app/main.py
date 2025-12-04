635f36f72a8a576d10e4df369e2299f27633366e
NEP-ENG\ehsan.ghorbani <ehsan.ghorbani@neptronic.com>
2025-12-04 10:28:21 -0500

    [implement echo fnc]

--- app/main.py
+++ app/main.py
@@ -8,14 +8,21 @@ def main():
          # TODO: Uncomment the code below to pass the first stage
         
         sys.stdout.write("$ ")
-        command = input()
+        command = input().strip()
         # pass
         if command == "exit":
             sys.exit(0)
-        if command == "echo":
-            print(input())
+        if command.startswith("echo"):
+            parts = command.split(" ", 1)
 
-            
+            if len(parts) == 1:
+                print("")
+            else:
+                print(parts[1])
+            continue
+
+
+
         print(f"{command}: command not found")
 
     
