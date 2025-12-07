e705aee5f9e05a540544b173c60be6e4e3654651
LAPTOP-G60CCHFV\Hanane <ehsan.contact@yahoo.com>
2025-12-07 14:59:50 -0500

    no message

--- app/main.py
+++ app/main.py
@@ -66,38 +66,12 @@ BUILTINs = {
 }
 
 def main():
-<<<<<<< HEAD
     while True:
         sys.stdout.write("$ ")
-<<<<<<< HEAD
-<<<<<<< HEAD
         command = input().strip()
  
         if not command:
             continue
-=======
-        command = input()
-<<<<<<< HEAD
-<<<<<<< HEAD
-        if command == "exit":
-            break
-        print(f"{command}: command not found")
-    sys.exit()
->>>>>>> origin/autofix-d4d6c55a-3fe1-4982-a92f-b7910dbb042a
-=======
-        
-        if command == "exit":
-            sys.exit(0)
-        
-        print(f"{command}: command not found")
->>>>>>> origin/autofix-1033e50b-13e7-4f34-8923-0ebd39a87659
-=======
-        
-        if command == "exit":
-            exit()
-        else:
-            print(f"{command}: command not found")
->>>>>>> origin/autofix-1c812e58-5f6d-4e24-96c4-42bf8f24c4bc
 
         parts = command.split()
         cmd = parts[0]
@@ -117,18 +91,5 @@ def main():
         print(f"{command}: command not found")
 
     
-=======
-        command = input()
-        if command == "exit":
-            sys.exit(0)
-        print(f"{command}: command not found")
-=======
-    sys.stdout.write("$ ")
-    command = input()
-    print(f"{command}: command not found")
->>>>>>> origin/autofix-beea1e22-aa50-49fc-bab5-65a9d078c494
-
-
->>>>>>> origin/autofix-34b55e0e-d9c5-4d57-8cfb-b150ff721a02
 if __name__ == "__main__":
     main()
